d08044c44c35eb37867c992d5aba17895b3a87ef
Wriell <wriellgamaoliveira@gmail.com>
2026-02-09 14:56:30 -0300

    Add files via upload
    Signed-off-by: Wriell <wriellgamaoliveira@gmail.com>

--- app.py
+++ app.py
@@ -349,5 +349,5 @@ def parse_xml(xml_content):
         return None
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
