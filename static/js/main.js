a627c90f9f24c6a8e8c16490b74b250e1a0cc495
Wriell <wriellgamaoliveira@gmail.com>
2026-02-10 13:54:47 -0300

    Add files via upload
    Signed-off-by: Wriell <wriellgamaoliveira@gmail.com>

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,6 @@
-
+// JS global do Troca XML Web
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.lucide && typeof window.lucide.createIcons === "function") {
+    window.lucide.createIcons();
+  }
+});
